2168147f9bba73b626b8fabd8037e8178d48e593
Crashededed <39970319+Crashededed@users.noreply.github.com>
2025-11-07 21:40:15 +0200

    added lab 1 files

--- lab_a/src/my_echo.c
+++ lab_a/src/my_echo.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-
-    // Printing the coundt of arguments
-  	printf("The value of argc is %d\n", argc);
-
     // Prining each argument
     for (int i = 0; i < argc; i++) {
-        printf("%s \n", argv[i]);
+        printf("%s ", argv[i]);
     }
 
     return 0;
