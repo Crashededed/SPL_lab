4fee96dd1f7f633dbc2cd1129229cf0857d35421
Ofri Gal <ofri0101@gmail.com>
2025-11-30 21:59:52 +0200

    finished part 1

---
+++ lab_B/src/hexaPrint.c
@@ -0,0 +1,33 @@
+// haxaPrint.c
+// Read a binary file given as argv[1] and print its bytes as hex to stdout.
+// Usage: haxaPrint <file>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int X = 16;
+void PrintHex(unsigned char* buffer, int length){
+    for (int i = 0; i < length; i ++){
+        printf("%02X ", buffer[i]);
+    }
+}
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 2;
+    }
+
+    FILE *f = fopen(argv[1], "rb");
+    if (!f) {
+        perror("fopen failed");
+        return 1;
+    }
+    unsigned char inbuf[X];
+    int bytes_read = 0;
+    while ((bytes_read = fread(inbuf, sizeof(unsigned char), X, f))){
+        PrintHex(inbuf, bytes_read);
+    }
+    printf("\n");
+    fclose(f);
+    return 0;
+}
