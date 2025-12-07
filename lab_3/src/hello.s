4f625b06da809d0a8cae19929ddeea2d387d65db
Crashededed <39970319+Crashededed@users.noreply.github.com>
2025-12-07 13:49:14 +0200

    lab 3 setup and task 0

---
+++ lab_3/src/hello.s
@@ -0,0 +1,18 @@
+global _start
+
+section .rodata
+ Str1: db "Hello World", 10, 0
+ DummyStr:
+section .data
+
+ outfile: dd 1 ; int outfile=1
+section .text
+
+_start: mov edx, DummyStr-Str1-1 ; Byte count
+ mov ecx, Str1
+ mov ebx, [outfile]
+ mov eax, 4
+ int 0x80 ; Linux system call
+ mov ebx, 0
+ mov eax, 1
+ int 0x80 ; Linux system call
